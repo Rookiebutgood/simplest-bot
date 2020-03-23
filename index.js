980e645b5c03dfacd3d0a5b23086cb3bca321c31
Rookiebutgood <demon-01011930@mail.ru>
2020-03-24 00:07:45 +0600

    Simplest bot"

---
+++ index.js
@@ -0,0 +1,37 @@
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const https = require("https");
+
+const bot = new Telegraf(BOT_KEY)
+
+bot.start( (ctx) => {
+  ({ ctx }) =>
+  ctx('', Markup
+    .keyboard(['Получить картинку'])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+} )
+
+
+bot.hears('Получить картинку', (ctx)=>{
+  const url = 'https://api.unsplash.com/photos/random?client_id='+PUBLIC_KEY;
+
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => {
+      body = JSON.parse(body);
+      ctx.replyWithPhoto(body.urls.full)
+    });
+  });
+})
+
+bot.launch()
+
+
